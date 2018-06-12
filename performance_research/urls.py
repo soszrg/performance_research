7ce41c8272811ca33192fd7a78aeed3df448edf7
zrg <zhangrg@mxchip.com>
2018-06-12 12:22:19 +0800

    添加performance接口

--- performance_research/urls.py
+++ performance_research/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from performance import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^performance/', views.performance),
 ]
